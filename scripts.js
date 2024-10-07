7c4038c9a78592efe166b59641fff40a9eac2492
Karen10Ruiz <163039934+Karen10Ruiz@users.noreply.github.com>
2024-10-07 13:28:05 -0600

    Listo mi ejercicio

---
+++ scripts.js
@@ -0,0 +1,22 @@
+//alert ("ENLAZADO wiwiwiwi");
+
+let nombre = "KAREN"
+let edad = 20
+let estudiaste = true; 
+prompt("¿Qué edad tienes")
+prompt("¿Cuál es tú nombre")
+
+
+console.log (nombre);
+console.log (edad);
+console.log (estudiaste);
+
+let saludo = `Hola soy ${nombre} y tengo ${edad} años`;
+console.log (saludo);
+
+
+if (estudiaste == true) {
+    alert (nombre + " estudiaste, pasarás la materia")
+} else if (estudiaste == false ) {
+    alert (nombre + " no estudiaste, reprobarás la materia")
+} 
